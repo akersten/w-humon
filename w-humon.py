9268c4c254ee6c5768e9d0ac112feb44f1958937
Alex Kersten <alex@kersten.email>
2016-02-27 11:35:40 -0600

    maybe this will work

--- w-humon.py
+++ w-humon.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 
-SCRIPT_NAME = '/humon'
-
-app = Flask(__name__)
+app = Flask(__name__,  static_url_path='/humon/static')
 app.config.from_object(__name__)
 
 @app.route('/')
@@ -12,4 +10,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4444)
+    app.run(host='0.0.0.0', port=4444,)
